fcd76c6bc4dbc1cab03ee62f633a0def4b6968ad
Apoena Oliveira Machado <machadoapoena@gmail.com>
2021-05-24 19:31:24 -0300

    configurando firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDiZDe2awEhlcEo0ghGN2UjTl922XNU3NI",
+    authDomain: "hummgourmet-ff632.firebaseapp.com",
+    databaseURL: "https://hummgourmet-ff632-default-rtdb.firebaseio.com",
+    projectId: "hummgourmet-ff632",
+    storageBucket: "hummgourmet-ff632.appspot.com",
+    messagingSenderId: "297390570812",
+    appId: "1:297390570812:web:33bec67444334844193239",
+    measurementId: "G-B84VK7DFCD"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDiZDe2awEhlcEo0ghGN2UjTl922XNU3NI",
+    authDomain: "hummgourmet-ff632.firebaseapp.com",
+    databaseURL: "https://hummgourmet-ff632-default-rtdb.firebaseio.com",
+    projectId: "hummgourmet-ff632",
+    storageBucket: "hummgourmet-ff632.appspot.com",
+    messagingSenderId: "297390570812",
+    appId: "1:297390570812:web:33bec67444334844193239",
+    measurementId: "G-B84VK7DFCD"
+  }
 };
 
 /*
